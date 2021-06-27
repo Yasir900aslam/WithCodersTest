dca0862b206111ed142c78c5c51d2365c930a6d1
yasir <yasir900aslam@gmail.com>
2021-06-28 02:19:58 +0500

    Async Search

--- components/searchandshow.tsx
+++ components/searchandshow.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Text, Input, Spacer, Button } from '@geist-ui/react';
+import { AutoComplete, Text, Input, Spacer, Button } from '@geist-ui/react';
 import { gql } from '@apollo/client';
 import client from '../utils/apollo-client';
 
 const SearchAndShow: React.FC = () => {
-  const [city, setCity] = useState(' ');
   const [temperature, setTemperature] = useState(' ');
   const [mintemperature, setMinTemperature] = useState(' ');
   const [maxtemperature, setMaxTemperature] = useState(' ');
   const [err, setErr] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const getDataOfCity = () => {
+  const getDataOfCity = (city) => {
     setLoading(true);
     setTemperature(' ');
     setMinTemperature(' ');
@@ -44,24 +43,21 @@ const SearchAndShow: React.FC = () => {
       })
       .catch((err) => {
         setLoading(false);
-        setErr(true);
       });
   };
 
   return (
     <>
-      <Input
-        label="City Name"
-        value={city}
-        placeholder="Enter City Name"
-        onChange={(e) => {
-          setCity(e.target.value);
+      <Spacer />
+      <Spacer />
+      <Text> Select City </Text>
+      <AutoComplete
+        placeholder="Select City"
+        clearable
+        onSearch={(value) => {
+          getDataOfCity(value);
         }}
       />
-      <Spacer />
-      <Button shadow type="secondary" onClick={getDataOfCity}>
-        Search
-      </Button>
       <Spacer />
       {loading !== false ? (
         <>
